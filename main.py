cfeeb14dbf7b45accf2b3a25b4dee8d1585e41fb
Amadeusz <Acer@LAPTOP-DCKI4AHO>
2023-07-28 17:35:44 +0200

    Second example

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import time
 
 done = False
 
+# Example 1
+
 
 def worker():
     counter = 0
@@ -21,13 +23,31 @@ def daemon_worker(text):
 
 
 worker_thread = threading.Thread(target=worker)
-
 daemon_worker_thread = threading.Thread(target=daemon_worker, daemon=True, args=("Value: ",))
-
 # daemon=True means that when all important threads will end we can end the program without waiting till this thread end
 
 # worker_thread.start()
-daemon_worker_thread.start()
+# daemon_worker_thread.start()
+
+
+# Example 2
+def timer1():
+    numbers = [1, 2, 4, 5, 7, 8, 10]
+    for number in numbers:
+        print(number)
+        time.sleep(1)
+
+
+def timer2():
+    numbers = [3, 6, 9]
+    for number in numbers:
+        print(number)
+        time.sleep(3)
+
+
+threading.Thread(target=timer1).start()
+threading.Thread(target=timer2).start()
+
 
 input("Press enter to quit")
 done = True
